f163eac21dd7395e559bfa6e27b94e3be8f219f9
Fletcher Rippon <fletcherrippon@gmail.com>
2021-08-14 17:39:14 +1000

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="text" v-model="color" />
+    <div class="user-input-color">
+      {{ color }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    color: 'white'
+  })
+}
+</script>
+
+<style scoped>
+.user-input-color {
+  background-color: v-bind(color)
+}
+</style>
